21aca724abed076695ac0c95aac5b18cf4cf7e00
Amâncio Zangui <114434987+Amanciozangui@users.noreply.github.com>
2023-03-11 01:59:54 +0100

    Add files via upload

---
+++ DES1.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite um Valor: "))
+n2 = int(input("Digite outro Valor:"))
+s = n1 + n2
+print("A Soma entré {0} e {1} vale {2}".format(n1, n2, s))
+print("Porra Caralho", s)
+n1 = int(input("Digite um Valor: "))
+n2 = int(input("Digite outro Valor:"))
+s = n1 + n2
+print("A Soma entré {0} e {1} vale {2}".format(n1, n2, s))
+
+
